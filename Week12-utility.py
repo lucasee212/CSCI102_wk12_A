0fdf55a9a3c829901a0ebe75772489feb2e14c43
Lucas Emery <lemery@mymail.mines.edu>
2019-11-18 17:11:39 -0700

    got that first function goin on

---
+++ Week12-utility.py
@@ -0,0 +1,8 @@
+        #   YOUR GITHUB REPO #https://github.com/lucasee212/CSCI102_wk12_A.git
+        #   Lucas Emery
+        #   ​CSCI 102 – Section A
+        #   Week 12 - Part A
+                                #1
+#simple print word
+def PrintOutput(word):
+    return print('OUTPUT %s' %word)
