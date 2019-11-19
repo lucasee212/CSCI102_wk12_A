2823afe21ef458094637e1ed3d536a1e997d57e7
Lucas Emery <lemery@mymail.mines.edu>
2019-11-18 17:16:14 -0700

    second function done

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,10 @@
 #simple print word
 def PrintOutput(word):
     return print('OUTPUT %s' %word)
+                                #2
+#function that calls file and reads the file
+def LoadFile(file):
+    openFile = open(file, 'r')
+    readFile = openFile.read()
+    openFile.close()
+    return readFile
